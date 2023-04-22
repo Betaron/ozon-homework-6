2824dd8f55e750b3feac1813d2ae8334bbeaab37
Иван Агабалаев <agabalaev01@yandex.ru>
2023-04-22 03:53:34 +0300

    Add project with console app which produce random generated good models into good_price_calc_requests topic

---
+++ src/Route256.Week6.Homework.GoodsGenerator/GoodsHelper.cs
@@ -0,0 +1,53 @@
+using Confluent.Kafka;
+using Route256.Week6.Homework.GoodsGenerator.Models;
+
+namespace Route256.Week6.Homework.GoodsGenerator;
+
+internal class GoodsHelper
+{
+    private long _idCounter;
+
+    public int From { get; set; }
+    public int To { get; set; }
+
+    public GoodsHelper(int from = 1, int to = 50)
+    {
+        _idCounter = 0;
+
+        From = from;
+        To = to;
+    }
+
+    public IEnumerable<GoodModel> GenerateGoods(int quantity)
+    {
+        var random = new Random();
+
+        for (int i = 0; i < quantity; i++)
+        {
+            _idCounter++;
+
+            yield return new GoodModel(
+                _idCounter,
+                random.Next(From, To),
+                random.Next(From, To),
+                random.Next(From, To),
+                random.Next(From, To));
+        }
+    }
+
+    public void GoodProduce(
+        IProducer<long, GoodModel> producer,
+        string topic,
+        GoodModel good,
+        CancellationToken token)
+    {
+        producer.ProduceAsync(
+            topic,
+            new Message<long, GoodModel>
+            {
+                Key = good.Id,
+                Value = good
+            },
+            token);
+    }
+}
---
+++ src/Route256.Week6.Homework.GoodsGenerator/Models/GoodModel.cs
@@ -0,0 +1,8 @@
+namespace Route256.Week6.Homework.GoodsGenerator.Models;
+
+internal record GoodModel(
+    long Id,
+    double Height,
+    double Length,
+    double Width,
+    double Weight);
